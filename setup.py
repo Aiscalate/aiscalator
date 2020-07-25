39d04c5c5f467470f843901ef1558c18a19c292e
requires.io <support@requires.io>
2020-07-25 03:10:34 +0200

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ description = 'AIscalate your Jupyter Notebook Prototypes ' \
 
 setup(
     name='aiscalator',
-    version='0.1.17',
+    version='0.1.18',
     license='Apache Software License 2.0',
     description=description,
     long_description='%s\n%s' % (
@@ -114,7 +114,7 @@ setup(
     install_requires=[
         # eg: 'aspectlib==1.1.1', 'six>=1.7',
         'Click>=6.0',
-        'jupytext>=1.1.7',
+        'jupytext>=1.5.2',
         'pyhocon>=0.3.48',
         'pytz>=2018.5',
     ],
